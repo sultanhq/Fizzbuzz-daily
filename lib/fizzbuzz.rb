029f9f401765e252064ebea5b80d939f8115ef7f
Rob Adams <robert.andrew.adams@gmail.com>
2016-11-09 09:44:24 +0000

    passed is / is not divisible by 3 & 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -5,3 +5,7 @@ end
 def is_divisible_by_5?(number)
   number % 5 == 0
 end
+
+def is_divisible_by_3_and_5?(number)
+  number % 3 == 0 && number % 5 == 0
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -17,4 +17,12 @@ describe 'fizzbuzz' do
     expect(is_divisible_by_5?(6)).to eq false
   end
 
+  it 'should test for is divisible by 3 & 5' do
+    expect(is_divisible_by_3_and_5?(15)).to eq true
+  end
+
+  it 'should test for is not divisible by 3 & 5' do
+    expect(is_divisible_by_3_and_5?(16)).to eq false
+  end
+
 end
