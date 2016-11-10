05a4b65d4037709427082a37a67d1dca238dc871
Rob Adams <robert.andrew.adams@gmail.com>
2016-11-10 09:00:54 +0000

    Final commit

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,15 +1,23 @@
 def is_divisible_by_3?(number)
-  number % 3 == 0
+  is_divisible_by?(number, 3)
 end
 
 def is_divisible_by_5?(number)
-  number % 5 == 0
+  is_divisible_by?(number, 5)
 end
 
-def is_divisible_by_3_and_5?(number)
-  number % 15 == 0
+def is_divisible_by_15?(number)
+  is_divisible_by?(number, 15)
 end
 
 def is_divisible_by?(number,divisor)
-  true
+  number % divisor == 0
+end
+
+def fizzbuzz(number)
+  return number if number == 0
+  return "fizzbuzz" if is_divisible_by_15?(number)
+  return "fizz" if is_divisible_by_3?(number)
+  return "buzz" if is_divisible_by_5?(number)
+  number
 end
