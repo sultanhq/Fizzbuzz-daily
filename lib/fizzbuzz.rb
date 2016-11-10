bdb3f709750f2bcac07b392888f5022d90084611
Rob Adams <robert.andrew.adams@gmail.com>
2016-11-10 09:50:34 +0000

    passed all tests fixing false positives

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,18 @@
+def is_divisible_by_3?(number)
+  number % 3 == 0
+end
+
+def is_divisible_by_5?(number)
+  number % 5 == 0
+end
+
+def is_divisible_by_15?(number)
+  is_divisible_by_3?(number) && is_divisible_by_5?(number)
+end
+
 def fizzbuzz(number)
-  return true if number == 3
-  return true if number == 5
-  return true if number == 15
-  false
+  return "fizzbuzz" if is_divisible_by_15?(number)
+  return "fizz" if is_divisible_by_3?(number)
+  return "buzz" if is_divisible_by_5?(number)
+  number
 end
