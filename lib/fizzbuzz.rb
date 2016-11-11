cb80b6d0f909323f55adb976c8a9f6b7dd14813e
Rob Adams <robert.andrew.adams@gmail.com>
2016-11-11 09:32:22 +0000

    Green tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,14 @@
+def is_divisible_by_15(number)
+  return true if is_divisible_by_3(number) && is_divisible_by_5(number)
+  false
+end
+
 def is_divisible_by_3(number)
-  return true if number == 3
+  return true if number % 3 == 0
+  false
+end
+
+def is_divisible_by_5(number)
+  return true if number % 5 == 0
   false
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,11 +1,22 @@
 require "./lib/fizzbuzz.rb"
 
 describe "Maths tests" do
-  it "test returns true if number is divisible by 3" do
+  it 'test returns true if number is divisible by 3' do
     expect(is_divisible_by_3(3)).to eq true
   end
-  it "test returns false if number is indivisible by 3" do
+  it 'test returns false if number is indivisible by 3' do
     expect(is_divisible_by_3(4)).to eq false
   end
+  it 'test returns true if number is divisible by 5' do
+    expect(is_divisible_by_5(5)).to eq true
+  end
+  it 'test returns false if number is indivisible by 5' do
+    expect(is_divisible_by_5(7)).to eq false
+  end
+  it 'test returns true if number is divisible by 3 & 5' do
+    expect(is_divisible_by_15(15)).to eq true
+  end
   
+
+
 end
